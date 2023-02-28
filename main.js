863b8f5396f86a8a7dad275f4baeae51f741fafb
giovannifranchi <giovanni.franchi95@gmail.com>
2023-02-28 19:28:33 +0100

    first commit

---
+++ main.js
@@ -0,0 +1,103 @@
+'use strict';
+
+
+const names = ['pino', 'paolo', 'piero', 'luca', 'gianni'];
+
+function likes(names){
+    if(names.length === 0){
+        console.log('nobody likes it!');
+    }else if(names.length === 1){
+        console.log(`${names[0]} likes this`);
+    }else if(names.length > 1 && names.length < 4){
+        let str = '';
+        for(let i = 0; i < names.length; i++){
+            if(names.length === 3 && i === 0){
+                str += `${names[i]}, `;
+            }else if(i === names.length - 1){
+                str += ` and ${names[i]} like this`;
+            }else {
+                str += names[i];
+            }
+        }
+        console.log(str);
+    }else {
+        let str = `${names[0]}, ${names[1]} and ${names.length - 2} others like this`;
+        console.log(str);
+    }
+}
+
+// likes(names);
+
+// PANGRAM EXERCISE
+
+// A pangram is a sentence that contains every single letter of the alphabet at least once. For example, the sentence "The quick brown fox jumps over the lazy dog" 
+// is a pangram, because it uses the letters A-Z at least once (case is irrelevant).
+
+// Given a string, detect whether or not it is a pangram. Return True if it is, False if not. Ignore numbers and punctuation.
+
+function isPangram(str){
+    const numberOfLetters = 26;
+    const singularChars = [];
+    const charArray = str.split('');
+    for(let char of charArray){
+        if(singularChars.includes(char) ||
+        isFinite(char) ||
+        char === ',' ||
+        char === '.' ||
+        char === ':' ||
+        char === ';' ||
+        char === '?' ||
+        char === '!' ||
+        char === '-' ||
+        char === '_' ||
+        char === ' ' ||
+        char === '/'){
+            continue;
+        }else {
+            singularChars.push(char.toLocaleLowerCase());
+        }
+    }
+    if(singularChars.length === numberOfLetters){
+        return true;
+    }else{
+        return false;
+    }
+}
+
+let str = 'The quick brown fox jumps over the lazy dog';
+
+// let checkstr = isPangram(str);
+
+// console.log(checkstr);
+
+
+
+// DESCRIPTION:
+// You might know some pretty large perfect squares. But what about the NEXT one?
+
+// Complete the findNextSquare method that finds the next integral perfect square after the one passed as a parameter. 
+// Recall that an integral perfect square is an integer n such that sqrt(n) is also an integer.
+
+// If the parameter is itself not a perfect square then -1 should be returned. You may assume the parameter is non-negative.
+
+
+function findNextSquare(number){
+    if(!Number.isInteger(Math.sqrt(number))){
+        return -1;
+    }else{
+        do{
+            number++;
+        }while((!Number.isInteger(Math.sqrt(number))))
+        return number;
+    }
+}
+
+let num = 25;
+
+// let next = findNextSquare(num);
+// console.log(next);
+
+
+
+
+
