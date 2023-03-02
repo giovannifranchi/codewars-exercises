18133cad08f46cdcefd0847c8fe94bba6fb1570e
giovannifranchi <giovanni.franchi95@gmail.com>
2023-03-02 13:46:19 +0100

    Feat: inArray exercise completed

--- main.js
+++ main.js
@@ -440,3 +440,47 @@ function uniqueInOrder(list){
 // let arr = [1,2,2,3,3];
 
 // console.log(uniqueInOrder(arr));
+
+
+
+
+// ----------------------------------------------
+
+
+
+// DESCRIPTION:
+// Given two arrays of strings a1 and a2 return a sorted array r in lexicographical order of the strings of a1 which are substrings of strings of a2.
+
+// Example 1:
+// a1 = ["arp", "live", "strong"]
+
+// a2 = ["lively", "alive", "harp", "sharp", "armstrong"]
+
+// returns ["arp", "live", "strong"]
+
+// Example 2:
+// a1 = ["tarp", "mice", "bull"]
+
+// a2 = ["lively", "alive", "harp", "sharp", "armstrong"]
+
+// returns []
+
+function inArray(array1, array2){
+    const finalArray = [];
+    for(let i = 0; i < array1.length; i++){
+        let substring = array1[i];
+        for(let j = 0; j < array2.length; j++){
+            let string = array2[j];
+            if(string.includes(substring) && !finalArray.includes(substring)){
+                finalArray.push(substring);
+            }
+        }
+    }
+    return finalArray.sort();
+}
+
+// const a1 = ["arp", "live", "strong"];
+// const a2 = ["lively", "alive", "harp", "sharp", "armstrong"];
+
+// console.log(inArray(a1, a2));
+
